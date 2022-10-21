ffe5f56ecff6ec9d395a67ec43963836739cb366
Martins Gouveia <gouveiamartins6@gmail.com>
2022-10-21 11:04:12 +0100

    ✨ Add CTA section

--- src/App.tsx
+++ src/App.tsx
@@ -135,7 +135,7 @@ function App() {
           </li>
         </section>
 
-        <section className="App__collection__discover">
+        <section className="App__collection__discover App__collection__discover--blur">
           <a href="#" className="App__collection__discover__title">
             <h1 className="App__collection__discover__title--name">Feel-Good</h1>
           </a>
@@ -156,7 +156,7 @@ function App() {
           </li>
         </section>
 
-        <section className="App__collection__discover">
+        <section className="App__collection__discover App__collection__discover--blur">
           <a href="#" className="App__collection__discover__title">
             <h1 className="App__collection__discover__title--name">Children & Family TV</h1>
           </a>
@@ -175,6 +175,21 @@ function App() {
               );
             })}
           </li>
+        </section>
+
+        <section className="App__collection__cta">
+          <div className="App__collection__cta__content">
+            <h1 className="App__collection__cta__content--title">There’s even more to watch.</h1>
+            <p className="App__collection__cta__content--subtitle">
+              Netflix has an extensive library of feature films, documentaries, TV shows, anime,
+              award-winning Netflix originals, and more. Watch as much as you want, anytime you
+              want.
+            </p>
+            <a href="#" className="App__collection__cta__content--btn">
+              {' '}
+              join now
+            </a>
+          </div>
         </section>
       </main>
     </div>
