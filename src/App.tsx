8b2c512dbe4ff27dc16e2743436fc65fd090f067
Martins Gouveia <gouveiamartins6@gmail.com>
2022-10-22 09:25:58 +0100

    🎨 Component footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
@@ -193,39 +195,7 @@ function App() {
         </section>
       </main>
 
-      <footer className="App__footer">
-        <div className="App__footer__content">
-          <a href="#" className="App__footer__content--help">
-            Questions? Contact us.
-          </a>
-
-          <ul className="App__footer__content__links">
-            <li className="App__footer__content__links--item">
-              <a href="#">FAQ</a>
-              <a href="#">Investor Relations</a>
-              <a href="#">Privacy</a>
-              <a href="#">Speed Test</a>
-            </li>
-            <li className="App__footer__content__links--item">
-              <a href="#">Help Center</a>
-              <a href="#">Jobs</a>
-              <a href="#">Cookie Preferences</a>
-              <a href="#">Legal Notices</a>
-            </li>
-            <li className="App__footer__content__links--item">
-              <a href="#">Account</a>
-              <a href="#">Ways to Watch</a>
-              <a href="#">Corporate Information</a>
-              <a href="#">Only on Netflix</a>
-            </li>
-            <li className="App__footer__content__links--item">
-              <a href="#">Media Center</a>
-              <a href="#">Terms of Use</a>
-              <a href="#">Contact Us</a>
-            </li>
-          </ul>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
