647eee7f209e0ca5e74ff765cb38d6d9ee9883ec
Martins Gouveia <gouveiamartins6@gmail.com>
2022-12-07 15:08:33 +0100

    ✅ Tests done in home

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,24 @@
-/// <reference types="cypress" />
-
 describe('Home', () => {
   it('should be open homepage', () => {
+    cy.viewport('macbook-13');
+
     cy.visit('/');
+  });
+
+  it('Element h1 should have class', () => {
+    cy.visit('/');
+
+    cy.get('h1').should('have.class', 'App__collection__content--title');
+  });
+
+  it('Homepage should have one list', () => {
+    cy.visit('/');
+
+    cy.get('.App__collection__discover__list')
+      .first()
+      .children()
+      .should(($lis) => {
+        expect($lis, '5 items').to.have.length(5);
+      });
   });
 });
