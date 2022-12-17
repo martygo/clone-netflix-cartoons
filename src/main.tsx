9c952fdfd72fee2d1c95258a06a4ae76fe5150c1
Martins Gouveia <gouveiamartins6@gmail.com>
2022-12-17 22:33:39 +0100

    🔧 Wrapper auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
+import { AuthProvider } from '@/context/AuthContext';
 
 import '@/App.scss';
-import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
